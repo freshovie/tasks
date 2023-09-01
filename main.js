aba238d4c76071bbe4ef1b1d235342b5a45182a9
freshovie <egorkeovie@gmail.com>
2023-09-01 14:39:23 +0100

    the message

---
+++ main.js
@@ -0,0 +1,224 @@
+// let variable
+
+// const name = "yewande"; //string
+// let id = 6 //integer
+
+// console.log(name + '')
+
+// let sum = function sum (a, b){
+//     return a + b;
+// }
+// 2+3; //5
+
+
+// let x = 5
+// let y = 3
+
+// //addition
+// console.log('x + y=', x+y);
+
+// //subtraction
+// console.log('x - y=',x-y);
+
+// //multiplication
+// console.log('x * y', x*y*x);
+
+// //division
+// console.log('x / y=', x/y);
+
+// //bodmas
+// console.log('x * y / x=', x*y/x)
+
+// const a =2, b =3; console.log(a > b); //true
+
+// console.log(2 == 2); //true
+// console.log(2 == '2'); //true
+
+
+    // let #abc = "abc";
+    // console.log(#abc);
+
+
+
+
+    // let_abc = "abc";
+    // console.log(_abc);
+
+    // //Enabling strict mode
+    // "use strict";
+    // //Defining variables of different cases
+    // let abc = "bcd"
+    // let ABC = "efg"
+    // console.log(abc);
+    // console.log(ABC);
+    // console.log(abc ==ABC);
+
+    // let class= "class";
+    // console.log(class);
+
+    // const age = windows.prompt (enter your age);
+    // if (age >= 18) {
+    //     console.log("nick is an adult.");
+    // }
+    // else {
+    //     console.log("nick is a child.");
+    // }
+
+    // const size = 18;
+    // if (size >= 18){
+    //     console.log("it is big.");
+    // }
+    // else {
+    //     console.log("it is small.")
+    // }
+
+
+    // let a = window.prompt("enter a number");
+    // let b = window.prompt("enter a Number");
+    // let c = window.prompt("enter a Number");
+    // a = typeof (Number);
+    // b = typeof (Number);
+    // c = typeof (Number);
+
+    // let result = a + b + c;
+    
+    // if (result == 12){
+    //     console.log("this is a dozen");
+    // }
+    // else{
+    //     console.log("this is just a number");
+    // }
+
+
+    // let sum(a + b + c) = 12;
+    // if{
+    //     (sum = 12 (prompt));
+    //     console.log("this is a dozen")  
+    // } 
+
+    // else{
+    //     (sum < 12 (prompt));
+    //     console.log("this is less")
+    // }
+
+    // parseInt
+
+    // const age = 51;
+
+    // if (age < 18) {
+    //     console.log("alice is under 18 years old.");
+    // }
+    // else if (age >= 18 && age <=21){
+    //     console.log("alice is between the ages of 18 and 21.");
+    // }
+    // else if (age >= 21 && age <=50){
+    //     console.log("alice is between the ages of 21 and 50years old.")
+    // }
+    // else {
+    //     console.log("alice is over 50years.")
+    // }
+
+    // const time = 22;
+
+    // if (time < 12){
+    //     console.log("Good Morning.")
+    // }
+    // else if (time >= 12 && time <= 18){
+    //     console.log("Good Afternoon")
+    // }
+    // else if (time >=18 && time <=24){
+    //     console.log("Good Night")
+    // }
+
+    // const variableNAme = parseInt(prompt("Enter time of day"));
+
+    // if (variableNAme < 10) {
+    //     console.log("Good Morning.")
+    // }
+    // else if (variableNAme < 20){
+    //     console.log("Good Afternoon.")
+    // }
+    // else{
+    //     console.log("Good Evening.")
+    // }
+
+    // const pet= "dog";
+
+    // if (pet === "lizard") {
+
+    // }
+
+
+    // const age = 22;
+    // const ownsCar = true;
+
+    // if (age >= 16 && ownsCar){
+    //     console.log("jerry is old enough to drive and has a car.");
+    // }
+    // else {
+    //     console.log("jerry does not drive.");
+    // }
+
+    // const boyfriendIsPaying = false;
+    // const isSale = true;
+
+    // if (boyfriendIsPaying || isSale){
+    //     console.log("Jesse will go shopping.")
+    // }
+    // else {
+    //     console.log("Jesse will not go shopping.");
+    // }
+
+    // Program to display text 5 times 
+    // const n = 5;
+
+//     //looping from i = 1 to 5
+//     for (let i=1; i<= n; i++) {
+//         console.log("I love Javascript.");
+//     }
+
+//     //program to display numbers from 1 to 5 const n =5;
+//     //looping from i =1 to 5
+//     // in each iteration, i is increased by 1 for (let i = 1; i <=n; i++)
+//     for let i
+//     {console.log("I love Javascript.");
+//     // printing the value of i 
+// }
+
+// let sum =0
+// const n = prompt("enter any number");
+
+// for (let i = 1; i <= n; i++)
+
+// let sum = 0;
+// const n =100
+
+
+// (let i = 1; i > 0; i++)
+// let i = 1, n=5;
+
+// let i = 1 to 5 while (i <= n)
+// {console.log (i); i += 1;}
+
+
+
+
+// [1, 2].forEach(alert)
+
+// alert("There will be an error")
+// [1, 2].forEach(alert)
+
+/*alert("All fine now");
+[1, 2].forEach(alert)
+ <Shift+Enter for a newline>
+// ...your code
+<Enter to run>
+
+
+let admin, name; // can declare two variables at once
+name = "John";
+admin = name;
+alert( admin ); // "John"
+
+*/
+
